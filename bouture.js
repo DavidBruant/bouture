785e4babd8f176aca0ca7321093fb49b8d6ce062
pntbr <stephane.langlois@scopyleft.fr>
2018-03-03 14:19:39 +0100

    Chaining properties and methods with multiple bouture calls

--- bouture.js
+++ bouture.js
@@ -51,12 +51,18 @@ const Bouture = {
 }
 const tagNames = new Set(['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'slot', 'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr'])
 
-tagNames.forEach(tag => {
-  Object.defineProperty(Bouture, tag, {
-    value: function (...args) {
-      let tags = [{name: tag, args: args}]
-      const Chained = {
-        getElement: function () {
+tagNames.forEach(tagName => {
+  Object.defineProperty(Bouture, tagName, {
+    get: function () {
+      let tags = [{name: tagName, args: []}]
+      const branche = function (...args) {
+        const lastTag = tags.pop()
+        tags.push({name: lastTag.name, args: args})
+        return branche
+      }
+
+      Object.defineProperty(branche, 'getElement', {
+        value: function () {
           let elements = []
           tags.forEach((tag, index) => {
             if (!index) {
@@ -67,20 +73,17 @@ tagNames.forEach(tag => {
           })
           return elements
         }
-      }
+      })
 
-      tagNames.forEach(tag => {
-        Object.defineProperty(Chained, tag, {
-          value: function (...args) {
-            tags.push({
-              name: tag,
-              args: args
-            })
-            return Chained
+      tagNames.forEach(tagName => {
+        Object.defineProperty(branche, tagName, {
+          get: function () {
+            tags.push({name: tagName, args: []})
+            return branche
           }
         })
       })
-      return Chained
+      return branche
     }
   })
 })
